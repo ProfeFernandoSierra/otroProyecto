5d1b9139cd9837368e22306829184e29492d39de
fernando sierra <r.sierra@profesor.duoc.cl>
2025-06-16 19:42:34 -0400

    feat/main: prepare 2 archivos y subi enunciado

---
+++ functions.py
@@ -0,0 +1,18 @@
+# La aplicación debe permitir al usuario:
+# 1.	Agregar un contacto: nombre, teléfono, email.
+# 2.	Listar contactos: mostrar todos los contactos guardados.
+# 3.	Buscar un contacto por nombre.
+# 4.	Eliminar un contacto.
+# 5.	Salir del programa.
+# Debe usarse un menú para navegar entre estas opciones.
+# La estructura de datos puede ser una lista de diccionarios.
+# ________________________________________
+# Organización del Trabajo
+# •	El repositorio debe ser creado por uno de los integrantes del grupo y compartido con los otros.
+# •	Todos deben clonar el repositorio en su computadora local.
+# •	Cada alumno trabajará en una rama propia (ejemplo: rama-juan, rama-maria, rama-carlos).
+# •	Cada uno será responsable de una parte del programa:
+# o	Alumno 1: Función para agregar y mostrar contactos.
+# o	Alumno 2: Función para buscar y eliminar contactos.
+# o	Alumno 3: Estructura del menú principal y control de flujo del programa.
+
