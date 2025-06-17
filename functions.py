538a89734ebea3ca0c58c331b8c3e49eb31eadd9
unknown <CETECOM@AOAWSC03LC0510.duoc.acad>
2025-06-16 20:08:14 -0400

    feat/dev/jesus: Codie el menu

--- functions.py
+++ functions.py
@@ -1,3 +1,36 @@
+def salida ():
+        print ("El programa se ha cerrado con exito :)")
+        return False
+
+
+
+
+
+def menu():
+    on = True
+    while on:
+        print("\t\tMENU AGENDA DE CONTACTOS\n")
+        print("1) Agregar un contacto: nombre, teléfono, email.")
+        print("2) Listar contactos: mostrar todos los contactos guardados.")
+        print("3) Buscar un contacto por nombre.")
+        print("4) Eliminar un contacto.")
+        print("5) Salir del programa.\n")
+        try:
+            opcion = int(input("Seleccione una opcion\n"))
+            if opcion == 1:
+                print("a")
+            elif opcion == 2:
+                print("a")
+            elif opcion == 3:
+                print("a")
+            elif opcion == 4:
+                print("a")
+            elif opcion == 5:
+               on = salida()
+        except:
+            print("Ingrese carecteres numericos")
+            
+            
 # La aplicación debe permitir al usuario:
 # 1.	Agregar un contacto: nombre, teléfono, email.
 # 2.	Listar contactos: mostrar todos los contactos guardados.
